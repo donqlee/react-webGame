d3aa0f82c99bc5492f8ca4d95b4ad8965e596ed1
donqlee <donqlee88@gmail.com>
2022-02-27 23:58:43 +0900

    wordRelay

--- WordRelay.jsx
+++ WordRelay.jsx
@@ -1,7 +1,7 @@
 const React = require('react');
-const { Component } = React;
+const { Component } = React; // 원래는 extends React.Component
 
-class WordRelay extends React.Component{
+class WordRelay extends Component{
     state = {
 
     };
