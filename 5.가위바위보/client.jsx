3979c4a488340be67dcf44a35cd81d27bff8be71
donqlee <donqlee88@gmail.com>
2022-03-11 17:10:22 +0900

    로또추첨기 class

--- 5.가위바위보/client.jsx
+++ 5.가위바위보/client.jsx
@@ -5,6 +5,7 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import RSPClass from './RSPClass';
 import RSPHooks from './RSPHooks';
+import Practice from './Practice';
 
 
 ReactDom.render(<RSPHooks />, document.querySelector('#root'));
---
+++ 6.로또추첨기/client.jsx
@@ -0,0 +1,11 @@
+// const React = require('react');
+// const ReactDom= require('react-dom');
+
+import React from 'react';
+import ReactDom from 'react-dom';
+import LottoClass from './LottoClass';
+import LottoHooks from './LottoHooks';
+
+
+
+ReactDom.render(<LottoClass />, document.querySelector('#root'));
